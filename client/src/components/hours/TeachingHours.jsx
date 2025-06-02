9c9ba2a84ccf4aaa9b537ff668f3caf14bdd0701
tammarh <38327516555@mby.co.il>
2025-06-02 21:51:39 +0300

    finish hour without create

--- client/src/components/hours/TeachingHours.jsx
+++ client/src/components/hours/TeachingHours.jsx
@@ -18,7 +18,7 @@ export default function TeachingHours({ globalFilter }) {
     const [selectedRow, setSelectedRow] = useState(null);
     const [updateVisible, setUpdateVisible] = useState(false)
     const toast = useRef(null);
-    const { control, handleSubmit, setValue, reset } = useForm({
+    const { control, handleSubmit, setValue, reset ,watch} = useForm({
         defaultValues: {
             _id: '',
             institution: '',
@@ -34,7 +34,7 @@ export default function TeachingHours({ globalFilter }) {
             actualQuota: 0,
         },
     });
-
+ 
     const grades = [
         { label: 'א', value: 'א' },
         { label: 'ב', value: 'ב' },
@@ -60,7 +60,7 @@ export default function TeachingHours({ globalFilter }) {
         { label: '25 - בסיסי חנמ רגיל לקויות קלות', value: '25 - בסיסי חנמ רגיל לקויות קלות' },
         { label: '16 - סל שחמ', value: '16 - סל שחמ' }
     ]
-    const designation = [
+    /*const designation = [
         { label: '11 - יוחא', value: '11 - יוחא' },
         { label: '85 - ניהול', value: '85 - ניהול' },
         { label: '1 - תוכניות לימודים', value: '1 - תוכניות לימודים' },
@@ -70,9 +70,33 @@ export default function TeachingHours({ globalFilter }) {
         { label: '90 - רופא', value: '90 - רופא' },
         { label: '91 - פרא רפואי', value: '91 - פרא רפואי' },
         { label: '99 - אחות', value: '99 - אחות' },
+    ]*/
 
-
+    const designationMap = {
+        '5 - סל עדיפות': [
+        { label: '11 - יוחא', value: '11 - יוחא' },
+        { label:  '85 - ניהול', value:  '85 - ניהול' }
+    ],
+    '6 - תקן בסיסי חנמ לקויות מורכבות': [
+        { label: '1 - תוכניות לימודים', value: '1 - תוכניות לימודים' },
+        { label: '18 - חינוך', value: '18 - חינוך' },
+        { label:  '85 - ניהול', value:  '85 - ניהול' },
+        { label:  '20 - יעוץ', value:  '20 - יעוץ' }
+    ],
+    '25 - בסיסי חנמ רגיל לקויות קלות': [
+        { label: '1 - תוכניות לימודים', value: '1 - תוכניות לימודים' },
+        { label: '18 - חינוך', value: '18 - חינוך' },
+        { label:  '85 - ניהול', value:  '85 - ניהול' },
+        { label:  '20 - יעוץ', value:  '20 - יעוץ' }
+    ], 
+    '16 - סל שחמ': [
+        { label: '90 - רופא', value: '90 - רופא' },
+        { label: '91 - פרא רפואי', value: '91 - פרא רפואי'},
+        { label: '99 - אחות', value: '99 - אחות' },
     ]
+    }
+    const sourceValue = watch('source');
+    const availableDesignations = designationMap[sourceValue] || [];
     const loudData = async () => {
         try {
             const data = await getAlltHours();
@@ -193,8 +217,77 @@ export default function TeachingHours({ globalFilter }) {
                     </div>
                 ) : (<></>)}
             </Dialog>
-
             <Dialog
+                visible={updateVisible}
+                modal
+                style={{ width: '30vw', direction: 'rtl' }}
+                onHide={() => {
+                    if (!updateVisible) return;
+                    setUpdateVisible(false);
+                }}
+            >
+                {selectedRow ? (
+                    <div className="p-4">
+                        <h3 className="dialog-title">
+                            עדכון שעות הוראה ל־{selectedRow.institution.institutionName}
+                        </h3>
+
+                        <form onSubmit={handleSubmit(updateRow)} className="p-fluid .small-input">
+                            {/* שדה כללי */}
+                            {[
+                                { name: "grade", label: "שכבה", component: Dropdown, options: grades },
+                                { name: "equivalent", label: "מקבילה", component: InputNumber },
+                                { name: "studentsAllocation", label: "מספר תלמידים", component: InputNumber, props: { min: 4, max: 15 } },
+                                { name: "classType", label: "סוג כיתה", component: Dropdown, options: classes },
+                                { name: "source", label: "מקור", component: Dropdown, options: source ,props: {
+                                    onChange: (e) => {
+                                        setValue('source', e.value)
+                                        setValue('designation', '')
+                                    }
+                                }},
+                                { name: "designation", label: "ייעוד", component: Dropdown, options: availableDesignations  },
+                                { name: "fromDate", label: "מתאריך", component: Calendar, props: { dateFormat: 'dd/mm/yy' } },
+                                { name: "untilDate", label: "עד תאריך", component: Calendar, props: { dateFormat: 'dd/mm/yy' } },
+                                { name: "calculatedQuota", label: "תקן מחושב", component: InputNumber },
+                                { name: "actualQuota", label: "תקן בפועל", component: InputNumber }
+                            ].map((fieldItem, index) => (
+                                <div className="p-field" style={{ marginBottom: '1rem' }} key={fieldItem.name}>
+                                    <label style={{ fontWeight: '500', color: '#34495e', marginBottom: '0.25rem', display: 'block' }}>
+                                        {fieldItem.label}
+                                    </label>
+                                    <Controller
+                                        name={fieldItem.name}
+                                        control={control}
+                                        render={({ field }) => {
+                                            const Component = fieldItem.component;
+                                            return (
+                                                <Component
+                                                    {...field}
+                                                    {...(fieldItem.options ? { options: fieldItem.options, optionLabel: "label", placeholder: fieldItem.label } : {})}
+                                                    {...(fieldItem.props || {})}
+                                                    value={field.value}
+                                                    onChange={(e) => field.onChange(e?.value ?? e?.target?.value)}
+                                                    className="p-inputtext"
+                                                />
+                                            );
+                                        }}
+                                    />
+                                </div>
+                            ))}
+
+                            <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+                                <Button
+                                    type="submit"
+                                    label="עדכון"
+                                    className="p-button-update"
+                                />
+                            </div>
+                        </form>
+                    </div>
+                ) : null}
+            </Dialog>
+
+            {/*<Dialog
                 visible={updateVisible}
                 modal
                 style={{ width: '30vw', direction: 'rtl' }}
@@ -359,7 +452,7 @@ export default function TeachingHours({ globalFilter }) {
 
                     </div >) : <></>
                 }
-            </Dialog >
+            </Dialog >*/}
         </div >
 
 
